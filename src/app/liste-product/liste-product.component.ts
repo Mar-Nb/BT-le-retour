238cfb3b61435043ec8b8c847559215c6369106b
Martin Niombela <m.niombela@cfa-insta.fr>
2021-03-17 17:26:21 +0100

    Amélioration du 2 partie 2, ajout du 3 partie 2

--- src/app/liste-product/liste-product.component.ts
+++ src/app/liste-product/liste-product.component.ts
@@ -15,13 +15,16 @@ export class ListeProductComponent implements OnInit {
   fruitsDeMer: any;  // Category : 1
   poissons: any; // Category : 0
 
-  valeur: any;
+  modifStock: any;
+  modifPromo: any;
 
   constructor(public prodService: ProductsService, public stockService: StockService) {
     this.produits = [];
     this.crustaces = [];
     this.fruitsDeMer = [];
     this.poissons = [];
+    this.modifStock = [];
+    this.modifPromo = [];
   }
 
   ngOnInit(): void {
@@ -36,46 +39,50 @@ export class ListeProductComponent implements OnInit {
 
   repartitionCategorie() {
     // Répartition dans les différents tableaux
-    this.produits.forEach(e => {
-      switch (e.category) {
+    this.produits.forEach(p => {
+      switch (p.category) {
         case 0:
-          this.poissons.push(e);
+          this.poissons.push(p);
           break;
         case 1:
-          this.fruitsDeMer.push(e);
+          this.fruitsDeMer.push(p);
           break;
         case 2:
-          this.crustaces.push(e);
+          this.crustaces.push(p);
           break;
       }
     });
   }
 
-  changerStock(type: string, i: number, id: number) {
+  stockModif(id: number, oldval: number) {
+    // Valeur récupérée dans le champ de modification de la page HTML
     const val = parseInt((document.getElementById("stock" + id) as HTMLInputElement).value);
-    if (val != null) {
-      let oldval: number;
-      switch (type) {
-        case "poisson":
-          oldval = this.poissons[i].quantityInStock;
-          if (val < 0 && -val < oldval) { this.stockService.diminuerStock(this.poissons[i].id, -val).subscribe(); }
-          else if (val > 0) { this.stockService.augmenterStock(this.poissons[i].id, val).subscribe(); }
-          break;
 
-        case "fruit":
-          oldval = this.fruitsDeMer[i].quantityInStock;
-          if (val < 0 && -val < oldval) { this.stockService.diminuerStock(this.fruitsDeMer[i].id, -val).subscribe(); }
-          else if (val > 0) { this.stockService.augmenterStock(this.fruitsDeMer[i].id, val).subscribe(); }
-          break;
-
-        case "crustace":
-          oldval = this.crustaces[i].quantityInStock;
-          if (val < 0 && -val < oldval) { this.stockService.diminuerStock(this.crustaces[i].id, -val).subscribe(); }
-          else if (val > 0) { this.stockService.augmenterStock(this.crustaces[i].id, val).subscribe(); }
-          break;
-      }
-    }
+    if (val != null && val != 0) { this.modifStock[id] = {"val": val, "oldval": oldval}; }
   }
 
-  rafraichir() { window.location.reload(); }
+  promoModif(id: number) {
+    // Valeur récupérée dans le champ de modification de la page HTML
+    const val = parseFloat((document.getElementById("promo" + id) as HTMLInputElement).value);
+
+    if (val != null && (val >= 0 && val <= 100)) { this.modifPromo[id] = val; }
+  }
+
+  envoyerDonnees() {
+    for (let id in this.modifStock) {
+      const val = this.modifStock[id]["val"];
+      const oldval = this.modifStock[id]["oldval"];
+      if (val < 0 && -val <= oldval) { this.stockService.diminuerStock(parseInt(id), -val).subscribe(); }
+      else if (val > 0) { this.stockService.augmenterStock(parseInt(id), val).subscribe(); }
+    }
+
+    for (let id in this.modifPromo) {
+      const val = this.modifPromo[id];
+      if (val == 0) { this.prodService.noPromo(parseInt(id)).subscribe(); }
+      else { this.prodService.setPromotion(parseInt(id), val).subscribe(); }
+    }
+
+    alert("Données envoyées !");
+    window.location.reload();
+  }
 }
